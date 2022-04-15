c10551127c3760756796af5e782fea17cec6736b
coolcoder93 <96700307+coolcoder93@users.noreply.github.com>
2022-04-15 21:52:22 +0200

    Update ben.py

--- ben.py
+++ ben.py
@@ -1,14 +1,18 @@
-from random import random
+from random import randint
 
 
 def talkingToBen():
     response = ""
     question = input("Ask a question: ")
-    randomresponse = random
-    if (randomresponse == 0):
+    randomresponse = randint(1, 4)
+    if (randomresponse == 1):
         response = "Yes"
-    else:
+    elif(randomresponse == 2):
         response = "No"
+    elif(randomresponse == 3):
+        response = "Hahahaha"
+    else:
+        response = "Ugh"
     print(f"You: \"{question}\"")
     print(f"Ben: \"{response}\"")
 
